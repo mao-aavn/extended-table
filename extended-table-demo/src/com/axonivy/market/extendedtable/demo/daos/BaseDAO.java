94206c23db4d0906beff4162bd64d23fa7b7eda3
Mao Nguyen <mao.nguyen@axonactive.com>
2025-09-05 17:00:55 +0700

    Fix typos in database configuration and persistence unit names

--- extended-table-demo/src/com/axonivy/market/extendedtable/demo/daos/BaseDAO.java
+++ extended-table-demo/src/com/axonivy/market/extendedtable/demo/daos/BaseDAO.java
@@ -6,6 +6,6 @@ package com.axonivy.market.extendedtable.demo.daos;
 public interface BaseDAO extends com.axonivy.utils.persistence.dao.BaseDAO {
 	@Override
 	default public String getPersistenceUnitName() {
-		return "entended_table_demo";
+		return "extended_table_demo";
 	}
 }
